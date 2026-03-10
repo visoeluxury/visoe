9ca6d14ae5581ba074c25e34fff29ac7f2311d03
visoeluxury <visoeluxury.com@gmail.com>
2026-03-10 19:08:23 +0700

    Delete app directory

--- app/routes/admin/_middleware.ts
+++
@@ -1,4 +0,0 @@
-import { basicAuth } from 'hono/basic-auth'
-export const middleware = [
-  basicAuth({ username: 'admin', password: 'password_rahasia' })
-]
--- app/server.ts
+++
@@ -1,9 +0,0 @@
-import { showRoutes } from 'hono/dev'
-import { createApp } from 'honox/server'
-
-const app = createApp()
-
-// Menampilkan daftar rute di terminal saat mode development (sangat membantu debugging)
-showRoutes(app)
-
-export default app
